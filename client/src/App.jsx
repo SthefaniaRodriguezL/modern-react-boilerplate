0542642e014a2e4200176f9207abb0cf2b35d371
SthefaniaRodriguezL <stherod13@gmail.com>
2019-05-07 20:22:12 -0500

    Fixed variables to async properly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,9 +10,9 @@ class App extends React.Component {
 		  <div>
         <h1>CSS GRADIENT</h1>
          <h3>Gradient Generator for Linear and Radial CSS Color Gradients</h3>
-         <div class="container">
+
           <Gradient />
-         </div>
+         
       </div>
     );
   }
--- client/src/Components/gradient.js
+++ client/src/Components/gradient.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+var randomColourOne=undefined
+var randomColourTwo=undefined
+
 class Gradient extends React.Component {
   constructor() {
     super();
@@ -11,45 +14,43 @@ class Gradient extends React.Component {
     this.colorOne = this.colorOne.bind(this);
     this.colorTwo = this.colorTwo.bind(this);
   }
+
    firstColor() {
-     console.log(this.prevState);
+
      var background = document.getElementById("myCanvas")
     background.style.backgroundImage = "-webkit-linear-gradient("+ this.state.colorOne +" , "+ this.state.colorTwo +")"
+    console.log("i am current stateeeeee when clicked" + this.state.colorOne);
   }
    colorOne() {
-     var randomColourOne = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)})
-     this.setState({colorOne: randomColourOne})
+      randomColourOne = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)})
      var background = document.getElementById("colorOne")
-     background.style.backgroundColor =  this.state.colorOne
+     background.style.backgroundColor =  randomColourOne
+     this.setState({
+       colorOne: randomColourOne})
    }
    colorTwo() {
-     var randomColourTwo = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)})
-     this.setState({colorTwo: randomColourTwo})
+     randomColourTwo = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)})
      var background = document.getElementById("colorTwo")
-     background.style.backgroundColor = this.state.colorTwo
-
+     background.style.backgroundColor = randomColourTwo
+     this.setState({
+       colorTwo: randomColourTwo})
    }
-
-   componentDidMount() {
-     this.colorOne()
-     this.colorTwo()
-
-   }
-
 
   render() {
+    console.log(this.state);
     return (
-      <div>
+      <div className='main'>
+      <div className='first'>
         <div>
           <div><h5>STYLE:</h5></div>
           <button type="Radial">Radial</button>
         </div>
         <div>
           <div><h5>DIRECTION:</h5></div>
-          <button type="right-direction">Top</button>
-          <button type="right-direction">Top Right</button>
-          <button type="right-direction">Right</button>
-          <button type="right-direction">Bottom right</button>
+          <button type="DirectionRight">Top</button>
+          <button type="DirectionRight">Top Right</button>
+          <button type="DirectionRight">Right</button>
+          <button type="DirectionRight">Bottom right</button>
         </div>
         <div>
           <button type="Bottom">Bottom</button>
@@ -57,6 +58,11 @@ class Gradient extends React.Component {
           <button type="Left">Left</button>
           <button type="TopLeft">Top left</button>
         </div>
+        <div>
+          <div><h5>COLOR FORMAT:</h5></div>
+          <button type="Hex">Hex</button>
+          <button type="Rgb">Rgb</button>
+         </div>
           <div>
             <div><h5>COLORS:</h5></div>
             <div id="colorOne"></div>
@@ -65,16 +71,13 @@ class Gradient extends React.Component {
             <button type="colorTwo" onClick={this.colorTwo}>colorTwo</button>
           </div>
             <button type="button" onClick={this.firstColor}>pick color</button>
-            <div class="parent" class="child">
-               <canvas id="myCanvas" width="200" height="100">
-               Your browser does not support the HTML5 canvas tag.
-               </canvas>
-             </div>
+          </div>
           <div>
-            <div><h5>COLOR FORMAT:</h5></div>
-            <button type="Hex">Hex</button>
-            <button type="Rgb">Rgb</button>
-           </div>
+            <canvas id="myCanvas">
+             Your browser does not support the HTML5 canvas tag.
+            </canvas>
+          </div>
+
         </div>
     );
   }
